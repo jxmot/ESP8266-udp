abced5afbd61536999c121ae5651c5f4f305be2b
jxmot <webdev@stupiddog.net>
2017-11-22 16:40:27 -0600

    initial commit, contains #define UDP_PACKET_SIZE

---
+++ udp-defs.h
@@ -0,0 +1,19 @@
+/* ************************************************************************ */
+/*
+    udp-defs.h - UDP #defines used by esp8266-udp.cpp and the client code.
+*/
+#ifndef UDPDEFS_H
+#define UDPDEFS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define UDP_PACKET_SIZE 75
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // UDPDEFS_H
+
